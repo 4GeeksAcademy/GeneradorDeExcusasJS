92f1efe7a201b7279b4ffbf2ddd3f0f56cdfd73e
Annams02 <155559840+Annams02@users.noreply.github.com>
2024-03-05 18:08:44 +0000

    generador excusas aleatorias

--- src/app.js
+++ src/app.js
@@ -2,10 +2,32 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  let numeroRandom = getRandomInt(4);
+
+  let frase =
+    who[numeroRandom] +
+    " " +
+    action[numeroRandom] +
+    " " +
+    what[numeroRandom] +
+    " " +
+    when[numeroRandom];
+
+  console.log(frase);
+
+  document.getElementById("excuse").innerHTML = frase;
 };
